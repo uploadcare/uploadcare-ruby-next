a1325be4554aafb81be2b8f98befebbbd7bb3052
Stepan Redka <stepan@railsmuffin.com>
2020-01-22 13:52:41 +0300

    feat: Uploader: add upload

--- lib/uploadcare/client/upload_client.rb
+++ lib/uploadcare/client/upload_client.rb
@@ -6,11 +6,19 @@ module Uploadcare
   class UploadClient < ApiStruct::Client
     upload_api
 
+    def upload(object, store: false)
+      if file?(object)           then upload_many([object], store: store)
+      elsif object.is_a?(Array)  then upload_many(object, store: store)
+      else
+        raise ArgumentError, "Expected input to be a file/Array/URL, given: `#{object}`"
+      end
+    end
+
     # https://uploadcare.com/api-refs/upload-api/#operation/baseUpload
-    
-    def upload_many(arr, **options)
+
+    def upload_many(arr, store: false)
       body = HTTP::FormData::Multipart.new(
-        upload_params(options[:store]).merge(files_formdata(arr))
+        upload_params(store).merge(files_formdata(arr))
       )
       post(path: 'base/',
            headers: { 'Content-type': body.content_type },
@@ -29,6 +37,10 @@ module Uploadcare
     def files_formdata(arr)
       arr.map { |file| [HTTP::FormData::File.new(file).filename,
                         HTTP::FormData::File.new(file)] }.to_h
+    end
+
+    def file?(object)
+      object.respond_to?(:path) && ::File.exist?(object.path)
     end
   end
 end
