9ba59b503e9db90fe5750a0c2e597d76ba857139
Stepan <stepan@railsmuffin.com>
2020-02-05 18:20:14 +0300

    feat: add webhook fields and live test data

--- lib/uploadcare/client/webhook_client.rb
+++ lib/uploadcare/client/webhook_client.rb
@@ -1,13 +1,29 @@
+# frozen_string_literal: true
+
 # https://uploadcare.com/api-refs/rest-api/v0.5.0/#tag/Webhook
 
 module Uploadcare
+  # client for webhook management
   class WebhookClient < ApiStruct::Client
     rest_api
+
+    # Create webhook
+    # https://uploadcare.com/docs/api_reference/rest/webhooks/#subscribe
+
+    def create(target_url, event: 'file.uploaded', is_active: true)
+      body = { 'target_url': target_url, 'event': event, 'is_active': is_active }.to_json
+      headers = AuthenticationHeader.call(method: 'POST', uri: '/webhooks/', content: body)
+      post(path: 'webhooks/', headers: headers, body: body)
+    end
+    # Returns array (not paginated list) of webhooks
+    # https://uploadcare.com/docs/api_reference/rest/webhooks/#get-list
 
     def list
       headers = AuthenticationHeader.call(method: 'GET', uri: '/webhooks/')
       get(path: 'webhooks/', headers: headers)
     end
+
+    # https://uploadcare.com/docs/api_reference/rest/webhooks/#unsubscribe
 
     def delete(name)
       body = { 'name': name }.to_json
@@ -15,9 +31,12 @@ module Uploadcare
       post(path: 'webhooks/unsubscribe/', headers: headers, body: body)
     end
 
-    def update(id)
-      headers = AuthenticationHeader.call(method: 'POST', uri: "/webhooks/#{id}/")
-      post(path: "webhooks/#{id}/", headers: headers)
+    # https://uploadcare.com/docs/api_reference/rest/webhooks/#subscribe-update
+
+    def update(id, **options)
+      body = options.to_json
+      headers = AuthenticationHeader.call(method: 'POST', uri: "/webhooks/#{id}/", content: body)
+      post(path: "webhooks/#{id}/", headers: headers, body: body)
     end
   end
 end
