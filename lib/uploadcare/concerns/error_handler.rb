ea06fc1a9d1a80898ad28973fbd4c21d38a50423
Stepan Redka <stepan@railsmuffin.com>
2020-02-17 17:51:06 +0300

    feat: ThrottleHandling for uploads

--- lib/uploadcare/concerns/error_handler.rb
+++ lib/uploadcare/concerns/error_handler.rb
@@ -5,20 +5,20 @@ module Uploadcare
   # raises errors instead of returning monads
   module ErrorHandler
     def failure(response)
-      parsed_response = JSON.parse(response.body.to_s)
       catch_throttling_error(response)
-      raise RequestError.new(parsed_response['detail'])
-    rescue JSON::ParserError => e
-      raise RequestError.new(response.status)
+      parsed_response = JSON.parse(response.body.to_s)
+      raise RequestError, parsed_response['detail']
+    rescue JSON::ParserError
+      raise RequestError, response.status
     end
 
     private
 
     def catch_throttling_error(response)
-      if response.code == 429
-        retry_after = response.headers['Retry-After'].to_i + 1 || 11
-        raise ThrottleError.new(retry_after), "Response throttled, retry #{retry_after} seconds later"
-      end
+      return unless response.code == 429
+      
+      retry_after = response.headers['Retry-After'].to_i + 1 || 11
+      raise ThrottleError.new(retry_after), "Response throttled, retry #{retry_after} seconds later"
     end
   end
 end
--- lib/uploadcare/service/upload_adapter.rb
+++ lib/uploadcare/service/upload_adapter.rb
@@ -6,22 +6,22 @@ module Uploadcare
   # This object decides which of upload methods to use.
   # Returns either file or array of files
   class UploadAdapter
+    extend Uploadcare::ThrottleHandler
     def self.call(object, **options)
-      case
-      when big_file?(object)
+      if big_file?(object)
         upload_big_file(object, **options)
-      when file?(object)
+      elsif file?(object)
         upload_file(object, **options)
-      when object.is_a?(Array)
+      elsif object.is_a?(Array)
         upload_files(object, **options)
-      when object.is_a?(String)
+      elsif object.is_a?(String)
         upload_from_url(object, **options)
       else
         raise ArgumentError, "Expected input to be a file/Array/URL, given: `#{object}`"
       end
     end
 
-    private
+    protected
 
     def self.upload_file(object, **options)
       response = UploadClient.new.upload_many([object], **options)
@@ -30,12 +30,12 @@ module Uploadcare
     end
 
     def self.upload_files(object, **options)
-      response = UploadClient.new.upload_many(object, **options)
+      response = handle_throttling { UploadClient.new.upload_many(object, **options) }
       handle_upload_errors(response)
       Hashie::Mash.new(files: response.success.map { |pair| { original_filename: pair[0], uuid: pair[1] } })
     end
 
-    def self.upload_big_file(object, **options)
+    def self.upload_big_file(object, **_options)
       response = MultipartUploadClient.new.upload(object)
       handle_upload_errors(response)
       File.new(response.success)
